7febb74fbe2283bb9441898c03046ec5969006e2
equis17 <1158582010@qq.com>
2022-04-01 22:04:08 +0800

    feat:bcm

--- hx_front/src/views/add/components/action.vue
+++ hx_front/src/views/add/components/action.vue
@@ -24,13 +24,9 @@
           <div class="rightCircles">
             <div v-for="item in 8" class="circle"></div>
           </div>
-          <div class="title">上海增值税普通发票</div>
           <div class="desc">税收分类编码版本号：40.0</div>
+          <div class="title">上海增值税普通发票</div>
           <div class="info">
-            <div class="item">
-              <div class="label">开票日期</div>
-              <div class="value">{{ today }}</div>
-            </div>
             <div class="item">
               <div class="label">发票代码：</div>
               <div class="value">031002100104</div>
@@ -39,69 +35,114 @@
               <div class="label">发票号码：</div>
               <div class="value">{{ form.serialNumber }}</div>
             </div>
+            <div class="item">
+              <div class="label">开票日期</div>
+              <div class="value">{{ today }}</div>
+            </div>
           </div>
           <div class="invoice">
             <div class="invoiceHeader">
               <div class="label">
-                购买方
+                <div class="labelWrap">
+                  <div v-for="item in '购买方'" class="word">{{ item }}</div>
+                </div>
               </div>
               <div class="content">
                 <div class="contentItem">
-                  <div class="contentLabel">名称：</div>
+                  <div class="contentLabel">
+                    <div v-for="item in '名   称'" class="contentLabelWord">{{ item }}</div>
+                    <span>：</span></div>
                   <div class="contentValue">
                     <input placeholder="请输入名称">
                   </div>
                 </div>
                 <div class="contentItem">
-                  <div class="contentLabel">纳税人识别号：</div>
+                  <div class="contentLabel">
+                    <div v-for="item in '纳税人识别号'" class="contentLabelWord">{{ item }}</div>
+                    <span>：</span></div>
                   <div class="contentValue">
                     <input placeholder="请输入纳税人识别号">
                   </div>
                 </div>
                 <div class="contentItem">
-                  <div class="contentLabel">地址、电话：</div>
+                  <div class="contentLabel">
+                    <div v-for="item in '地址、电话'" class="contentLabelWord">{{ item }}</div>
+                    <span>：</span></div>
                   <div class="contentValue">
                     <input placeholder="请输入地址、电话">
                   </div>
                 </div>
                 <div class="contentItem">
-                  <div class="contentLabel">开户行及账号：</div>
+                  <div class="contentLabel">
+                    <div v-for="item in '开户行及账号'" class="contentLabelWord">{{ item }}</div>
+                    <span>：</span></div>
                   <div class="contentValue">
                     <input placeholder="请输入开户行及账号">
                   </div>
                 </div>
               </div>
-              <div class="label">
-                密码区
+              <div class="label" style="border-left: 2px solid #843400">
+                <div class="labelWrap">
+                  <div v-for="word in '密码区'" class="word">{{ word }}</div>
+                </div>
               </div>
-              <div class="content">
+              <div class="content" style="flex:1">
 
               </div>
             </div>
             <div class="invoiceBody">
-              <div class="table">
-                <table border="1" cellspacing="0">
-                  <tr>
-                    <th>税务或应税劳务、服务名称</th>
-                    <th>规格型号</th>
-                    <th>单位</th>
-                    <th>数量</th>
-                    <th>单价（不含税）</th>
-                    <th>金额（不含税）</th>
-                    <th>税率</th>
-                    <th>税额</th>
-                  </tr>
-                  <tr v-for="item in form.cargoList">
-                    <th>{{ item.timestamp }}</th>
-                    <th>{{ item.amount }}</th>
-                    <th>{{ item.accountNum }}</th>
-                    <th>{{ item.accountName }}</th>
-                    <th>1296023347888525312</th>
-                    <th>北京加旺电器有限公司</th>
-                    <th>{{ item.abstractContent }}</th>
-                    <th>{{ item.creationDate }}</th>
-                  </tr>
-                </table>
+              <div class="bodyWrapper">
+                <div class="table">
+                  <table border="1" cellspacing="0">
+                    <tr class="header">
+                      <th>税务或应税劳务、服务名称</th>
+                      <th>规格型号</th>
+                      <th>单位</th>
+                      <th>数量</th>
+                      <th>单价（不含税）</th>
+                      <th>金额（不含税）</th>
+                      <th>税率</th>
+                      <th>税额</th>
+                    </tr>
+                    <tr v-for="item in form.cargoList" class="row">
+                      <th>{{ item.timestamp }}</th>
+                      <th>{{ item.amount }}</th>
+                      <th>{{ item.accountNum }}</th>
+                      <th>{{ item.accountName }}</th>
+                      <th>1296023347888525312</th>
+                      <th>北京加旺电器有限公司</th>
+                      <th>{{ item.abstractContent }}</th>
+                      <th>{{ item.creationDate }}</th>
+                    </tr>
+                  </table>
+                </div>
+                <div class="bodyFooter">
+                  <div class="content">
+                    <div class="contentWrap">
+                      <div v-for="item in '合计'" class="label">{{ item }}</div>
+                    </div>
+                  </div>
+                  <div class="money">
+                    ￥0.00
+                  </div>
+                  <div class="money">
+                    ￥0.00
+                  </div>
+                </div>
+                <div class="bodySum">
+                  <div class="left">
+                    价税合计（大写）
+                  </div>
+                  <div class="right">
+                    <div class="rightWrap">
+                      <div class="upper">123</div>
+                      <div class="small">
+                        <span class="color">（小写）</span>
+                        <span class="price">￥0.00</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <!--TODO-->
@@ -109,36 +150,52 @@
             <div class="invoiceFooter">
               <div class="invoiceHeader">
                 <div class="label">
-                  销售方
+                  <div class="labelWrap">
+                    <div v-for="item in '销售方'" class="word">{{ item }}</div>
+                  </div>
                 </div>
                 <div class="content">
                   <div class="contentItem">
-                    <div class="contentLabel">名称：</div>
+                    <div class="contentLabel">
+                      <div v-for="item in '名称'" class="contentLabelWord">{{ item }}</div>
+                      <span>：</span>
+                    </div>
                     <div class="contentValue">
                       <input placeholder="请输入名称">
                     </div>
                   </div>
                   <div class="contentItem">
-                    <div class="contentLabel">纳税人识别号：</div>
+                    <div class="contentLabel">
+                      <div v-for="item in '纳税人识别号'" class="contentLabelWord">{{ item }}</div>
+                      <span>：</span>
+                    </div>
                     <div class="contentValue">
                       <input placeholder="请输入纳税人识别号">
                     </div>
                   </div>
                   <div class="contentItem">
-                    <div class="contentLabel">地址、电话：</div>
+                    <div class="contentLabel">
+                      <div v-for="item in '地址、电话'" class="contentLabelWord">{{ item }}</div>
+                      <span>：</span>
+                    </div>
                     <div class="contentValue">
                       <input placeholder="请输入地址、电话">
                     </div>
                   </div>
                   <div class="contentItem">
-                    <div class="contentLabel">开户行及账号：</div>
+                    <div class="contentLabel">
+                      <div v-for="item in '开户行及账号'" class="contentLabelWord">{{ item }}</div>
+                      <span>：</span>
+                    </div>
                     <div class="contentValue">
                       <input placeholder="请输入开户行及账号">
                     </div>
                   </div>
                 </div>
-                <div class="label">
-                  备注
+                <div class="label" style="border-left: 2px solid #843400;">
+                  <div class="labelWrap">
+                    <div v-for="item in '备注'" class="word">{{ item }}</div>
+                  </div>
                 </div>
                 <div class="content">
                 </div>
@@ -170,11 +227,6 @@ import dayjs from 'dayjs'
 
 export default {
   name: 'action',
-  computed: {
-    today() {
-      return dayjs().format('YYYY年MM月DD日')
-    },
-  },
   data() {
     return {
       form: {
@@ -182,6 +234,11 @@ export default {
         cargoList: [{}],
       },
     }
+  },
+  computed: {
+    today() {
+      return dayjs().format('YYYY年MM月DD日')
+    },
   },
   methods: {
     async getSerialNumber() {
@@ -205,7 +262,7 @@ export default {
 
   .mainContainer {
     display: flex;
-    height: 100%;
+    height: calc(100% - 48px);
     margin-top: 10px;
     gap: 20px;
 
@@ -217,7 +274,7 @@ export default {
         position: relative;
         width: 100%;
         height: 100%;
-        padding: 0px 55px 0 55px;
+        padding: 12px 55px 0 65px;
 
         .circle {
           width: 25px;
@@ -233,7 +290,7 @@ export default {
           display: flex;
           flex-direction: column;
           justify-content: space-between;
-          height: 100%;
+          height: calc(100% - 48px);
         }
 
         .rightCircles {
@@ -243,7 +300,260 @@ export default {
           display: flex;
           flex-direction: column;
           justify-content: space-between;
+          height: calc(100% - 48px);
+        }
+
+        .title {
+          font-size: 30px;
+          line-height: 42px;
+          text-align: center;
+          color: #00F;
+        }
+
+        .desc {
+          float: right;
+          margin-top: 20px;
+          color: #CCC;
+        }
+
+        .info {
+          font-size: 0;
+          display: flex;
+          margin-top: 10px;
+          gap: 12px;
+
+          .item {
+            font-size: 0;
+
+            &:last-of-type {
+              flex: 1;
+              text-align: right;
+
+              .value {
+                color: #000;
+              }
+            }
+
+            .label {
+              font-family: PingFangSC, PingFangSC-Medium;
+              font-size: 16px;
+              font-weight: 500;
+              line-height: 22px;
+              display: inline-block;
+              text-align: center;
+              vertical-align: middle;
+              color: #843400;
+            }
+
+            .value {
+              font-family: PingFangSC, PingFangSC-Medium;
+              font-size: 14px;
+              font-weight: 500;
+              line-height: 20px;
+              display: inline-block;
+              margin-left: 12px;
+              text-align: center;
+              vertical-align: middle;
+              color: #C03;
+            }
+          }
+        }
+
+        .invoice {
+          display: flex;
+          flex-direction: column;
+          height: calc(100% - 148px);
+          margin-top: 14px;
+          border: 2px solid #843400;
+
+          .invoiceBody {
+            flex: 1;
+            border-bottom: 2px solid #843400;
+
+            .bodyWrapper {
+              display: flex;
+              flex-direction: column;
+              width: 100%;
+              height: 100%;
+            }
+          }
+
+          .invoiceHeader {
+            display: flex;
+            border-bottom: 2px solid #843400;
+
+
+            .label {
+              width: 40px;
+              padding: 8px;
+              text-align: center;
+              border-right: 2px solid #843400;
+
+              .labelWrap {
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+                justify-content: space-between;
+                width: 100%;
+                height: 100%;
+                justify-items: center;
+
+                .word {
+                  font-size: 13px;
+                  color: #843400;
+                }
+              }
+            }
+
+
+            .content {
+              flex: 2;
+
+              .contentItem {
+                font-size: 14px;
+                display: flex;
+                align-items: center;
+                width: 100%;
+                padding-left: 4px;
+                color: #843400;
+
+                .contentLabel {
+                  display: flex;
+                  justify-content: space-between;
+                  width: 120px;
+
+                  .contentLabelWord {
+                    flex: 1;
+                  }
+                }
+
+                .contentValue {
+                  flex: 1;
+
+                  input {
+                    font-size: 14px;
+                    //去除input原生
+                    width: 100%;
+                    border: none;
+                    outline: none;
+                    background: none;
+                    //placeholder颜色
+                    &::-webkit-input-placeholder {
+                      color: #CCC;
+                    }
+
+                  }
+                }
+              }
+
+            }
+
+            &:last-of-type {
+              border-bottom: none;
+            }
+          }
+        }
+      }
+    }
+
+    .invoiceBody {
+      .table {
+        overflow-y: auto;
+        flex: 1;
+        width: 100%;
+
+        .header {
+          border-color: #CCC;
+          background-color: aliceblue;
+
+          th {
+            font-weight: 400;
+            padding: 4px 0;
+          }
+        }
+
+        .row {
+          border-color: aliceblue;
+
+          th {
+            font-weight: 400;
+            padding: 4px 0;
+          }
+        }
+
+        table {
+          width: 100%;
+          border-color: aliceblue;
+        }
+      }
+
+      .bodyFooter {
+        display: flex;
+        padding: 4px 60px 4px;
+
+        .content {
+          flex: 1;
+          justify-content: center;
+
+          .contentWrap {
+            font-size: 13px;
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            color: #843400;
+          }
+        }
+
+        .money {
+          font-size: 13px;
+          flex: 1;
+          text-align: center;
+          color: #000;
+        }
+      }
+
+      .bodySum {
+        display: flex;
+        border-top: 2px solid #843400;
+
+        .left {
+          font-size: 13px;
+          width: 200px;
+          padding: 4px 0;
+          text-align: center;
+          color: #843400;
+          border-right: 2px solid #843400;
+        }
+
+        .right {
+          flex: 1;
           height: 100%;
+
+          .rightWrap {
+            display: flex;
+            align-items: center;
+            height: 100%;
+
+            .upper {
+              font-weight: 400;
+              flex: 1;
+              padding-left: 40px;
+            }
+
+            .small {
+              width: 200px;
+
+              .color {
+                font-size: 13px;
+                color: #843400;
+              }
+
+              .price {
+                font-size: 13px;
+                color: #000;
+              }
+            }
+          }
         }
       }
     }
